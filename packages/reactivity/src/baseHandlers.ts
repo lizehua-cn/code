808bcfbfc078cdfef545d52f0cd9d450992543e4
李泽华 <geforce96@hotmail.com>
2022-10-31 21:28:07 +0800

    响应式属性还是对象的处理

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,5 +1,6 @@
+import { isObject } from '@vue/shared'
 import { track, trigger } from './effect'
-import { ReactiveFlags } from './reactive'
+import { reactive, ReactiveFlags } from './reactive'
 export const mutableHandles = {
   get(target, key, receiver) {
     // FIXME 会改变this指向
@@ -7,7 +8,13 @@ export const mutableHandles = {
     // 可以通过此字段判断是否是被代理对象
     if (key === ReactiveFlags.IS_REACTIVE) return true
     track(target, key)
-    return Reflect.get(target, key, receiver)
+    const r = Reflect.get(target, key, receiver)
+    if (isObject(r)) {
+      // 如果是对象, 重新注册为响应式
+      // 取值时才会触发, 性能优化
+      return reactive(r)
+    }
+    return r
   },
   set(target, key, value, receiver) {
     let oldVal = target[key]
